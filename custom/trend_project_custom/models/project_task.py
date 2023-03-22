69a45a4d8773b1d41de151102e378f31054b0536
Eslam <eslam.rezk96@hotmail.com>
2023-03-22 10:11:04 +0200

    [update] move line domian in task

--- custom/trend_project_custom/models/project_task.py
+++ custom/trend_project_custom/models/project_task.py
@@ -12,17 +12,3 @@ class Task(models.Model):
     def onchange_paid_by_us(self):
         if not self.paid_by_us:
             self.move_line_ids = False
-            return
-        ids = self._calc_move_line_ids_domain()
-        return {
-            "domain": {"move_line_ids": [("id", "in", ids)]}
-        }
-
-    def _calc_move_line_ids_domain(self):
-        cash_bank_move_lines = self.env["account.move.line"].search([
-            ("account_id.account_type", "=", "asset_cash"),
-            ("move_id.state", "=", "posted"),
-            ("move_id.project_id", "=", self.project_id.id)
-        ])
-        return cash_bank_move_lines.ids
-
