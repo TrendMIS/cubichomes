4f9616867b7e0d002dcf07638b15ab9172067bfc
Eslam <eslam.rezk96@hotmail.com>
2023-03-22 10:17:35 +0200

    [update]account payment post

--- custom/trend_project_custom/models/account_payment.py
+++ custom/trend_project_custom/models/account_payment.py
@@ -6,3 +6,7 @@ class AccountPayment(models.Model):
 
     project_id = fields.Many2one('project.project')
     analytic_account_id = fields.Many2one(related='project_id.analytic_account_id', store=True)
+
+    def action_post(self):
+        super().action_post()
+        self.move_id.project_id = self.project_id.id
