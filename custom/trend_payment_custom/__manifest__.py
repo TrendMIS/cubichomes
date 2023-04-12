8b38d817bd26cc31c6dac7c48d9d2f602ae95630
Eslam <eslam.rezk96@hotmail.com>
2023-04-12 10:51:08 +0200

    [add] journal items in payment screen, task_id:1213

---
+++ custom/trend_payment_custom/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Trend Payment Custom",
+
+    'summary': """""",
+
+    'author': "Trend Development Team",
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        'views/account_payment.xml',
+    ],
+}
