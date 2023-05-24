49472eb3e4ee9b61f7abde5dab779033a5544bbf
Eslam <eslam.rezk96@hotmail.com>
2023-05-24 17:58:01 +0300

    [add] product attribute in home and shop card, task_id:1330

---
+++ custom/trend_website_product_card/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Trend Website Product Card",
+
+    'summary': """""",
+
+    'author': "Trend Development Team",
+    # any module necessary for this one to work correctly
+    'depends': ['trend_advanced_search_website'],
+
+    # always loaded
+    'data': [
+        'views/shop_product_card.xml',
+        'views/home_product_card.xml',
+    ],
+}
