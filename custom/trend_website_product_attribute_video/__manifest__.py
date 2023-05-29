9c851cf72cad0915d527ea071a559b65ae46f278
Eslam <eslam.rezk96@hotmail.com>
2023-05-29 11:48:14 +0300

    [add] product video and attribute icons, task_id: 1331,1332

---
+++ custom/trend_website_product_attribute_video/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Product Attribute Video",
+
+    'summary': """""",
+
+    'author': "Trend Development Team",
+    # any module necessary for this one to work correctly
+    'depends': ['website_sale'],
+
+    # always loaded
+    'data': [
+        'views/product_card.xml',
+        'views/product_template.xml',
+    ],
+}
