90150f748e1e6a53701433729fea3053b5238599
Eslam <eslam.rezk96@hotmail.com>
2023-10-15 10:25:27 +0300

    show company address in invoice report

--- custom/sales_amount_discount/models/res_company.py
+++ custom/sales_amount_discount/models/res_company.py
@@ -6,4 +6,4 @@ from odoo import models, fields
 class Company(models.Model):
     _inherit = 'res.company'
 
-    arabic_name = fields.Char()
+    arabic_name = fields.Char(related='partner_id.arabic_name')
