a89abec8ca940f2048e55a7609160c2aa41f6385
Eslam <eslam.rezk96@hotmail.com>
2023-11-05 13:23:02 +0200

    Cover snippet in product page, task_id:1992

---
+++ custom/trend_website_product_section_hidden/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Trend Website Product Section Hidden",
+
+    'summary': """""",
+
+    'author': "Trend Development Team",
+    # any module necessary for this one to work correctly
+    'depends': ['website_sale'],
+
+    # always loaded
+    'data': [
+        'views/product_template.xml',
+    ],
+}
