bd6961781390212522f4356c7d69454f1fcfa777
Eslam <eslam.rezk96@hotmail.com>
2023-11-05 15:57:27 +0200

    product inspiration, task_id:1994

---
+++ custom/trend_website_product_inspiration/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Trend Website Product Inspiration",
+
+    'summary': """""",
+
+    'author': "Trend Development Team",
+    # any module necessary for this one to work correctly
+    'depends': ['website_sale'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_template.xml',
+        'views/template.xml',
+    ],
+}
