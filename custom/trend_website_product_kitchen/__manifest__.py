6a37490ec79886b44a6685bcca251abd240d65dc
Eslam <eslam.rezk96@hotmail.com>
2023-11-07 09:34:00 +0200

    kitchen design, task_id:2068

---
+++ custom/trend_website_product_kitchen/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Trend Website Product Kitchen",
+
+    'summary': """""",
+
+    'author': "Trend Development Team",
+    # any module necessary for this one to work correctly
+    'depends': ['trend_website_product_page'],
+
+    # always loaded
+    'data': [
+        'views/product_template.xml',
+        'views/template.xml',
+    ],
+}
