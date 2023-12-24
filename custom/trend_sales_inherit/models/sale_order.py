508184b45016d2b739cec7297b227b13d6495fac
Eslam <eslam.rezk96@hotmail.com>
2023-12-24 15:41:59 +0200

    validation total with confirm order

--- custom/trend_sales_inherit/models/sale_order.py
+++ custom/trend_sales_inherit/models/sale_order.py
@@ -112,10 +112,12 @@ class SaleOrder(models.Model):
     def validate_total_service_amount(self):
         if self.sale_id: return  # skip validation for child sales orders
         design_lines = self.order_line.filtered(lambda l: l.internal_service_type == "design")
-        if sum(design_lines.mapped("price_total")) != self.design_amount:
+        print('design_amount', int(self.design_amount))
+        print('price_total', int(sum(design_lines.mapped("price_total"))))
+        if int(sum(design_lines.mapped("price_total"))) != int(self.design_amount):
             raise ValidationError("Design lines total price is different than the total design amount")
         supervision_lines = self.order_line.filtered(lambda l: l.internal_service_type == "supervision")
-        if sum(supervision_lines.mapped("price_total")) != self.supervision_amount:
+        if int(sum(supervision_lines.mapped("price_total"))) != int(self.supervision_amount):
             raise ValidationError("Supervision lines total price is different than the total supervision amount")
 
 
